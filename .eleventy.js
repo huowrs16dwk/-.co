a1475fe55a7dfb569103780591dbb54ae70c4eb0
浪人 <huowrs16dwk@proton.me>
2023-08-25 18:29:06 +0000

    styles

--- .eleventy.js
+++ .eleventy.js
@@ -2,5 +2,14 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addCollection("posts", function (collectionApi) {
     return collectionApi.getFilteredByGlob("posts/*.md");
   });
+
+  eleventyConfig.addFilter('dateFormat', function(value) {
+    const date = new Date(value);
+    const yyyy = date.getFullYear();
+    const mm = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+    const dd = String(date.getDate()).padStart(2, '0');
+    return `${yyyy}-${mm}-${dd}`;
+  });
+
 };
 
--- index.md
+++ index.md
@@ -1,10 +1,6 @@
-<h1>浪人.co</h1>
+---
+layout: default
+---
 
-<nav>
-  <ul>
-    {% for post in collections.posts reversed %}
-      <li><a href="{{ post.url }}">{{ post.data.title }} ({{ post.data.date }})</a></li>
-    {% endfor %}
-  </ul>
-</nav>
+# {{ title }}
 
